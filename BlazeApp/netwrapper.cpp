7efa1d41bbe9f507b8d4f70f15c595ae9cbca88f
Connor Shearer <connor_shearer@outlook.com>
2020-08-23 19:26:08 +0100

    Add remaining FFmpeg WebM stuff

--- BlazeApp/netwrapper.cpp
+++ BlazeApp/netwrapper.cpp
@@ -1,17 +1,28 @@
 #include "pch.h";
 #include "netwrapper.h";
 
-string DotNetWrapper::DOTNET_MINIFY_JS(string content)
+//
+
+string DotNetWrapper::DOTNET_MINIFY_JS(string& content)
 {
 	return (*reinterpret_cast<string(*)(string&)>(get_lib_function(get_library(Globals::LIB_NET_WRAPPER), "minify_js")))(content);
 }
 
-string DotNetWrapper::DOTNET_MINIFY_CSS(string content)
+string DotNetWrapper::DOTNET_MINIFY_CSS(string& content)
 {
 	return (*reinterpret_cast<string(*)(string&)>(get_lib_function(get_library(Globals::LIB_NET_WRAPPER), "minify_css")))(content);
 }
 
-string DotNetWrapper::DOTNET_DOWNLOAD_STRING(string content)
+//
+
+void DotNetWrapper::DOTNET_CONVERT_VIDEO_TO_WEBM(string& input_path, string& output_path, int& bitrate)
+{
+	(*reinterpret_cast<void(*)(string&, string&, int&)>(get_lib_function(get_library(Globals::LIB_NET_WRAPPER), "convert_video_to_webm")))(input_path, output_path, bitrate);
+}
+
+//
+
+string DotNetWrapper::DOTNET_DOWNLOAD_STRING(string& content)
 {
 	return (*reinterpret_cast<string(*)(string&)>(get_lib_function(get_library(Globals::LIB_NET_WRAPPER), "download_data")))(content);
 }
--- BlazeApp/netwrapper.h
+++ BlazeApp/netwrapper.h
@@ -3,7 +3,19 @@
 class DotNetWrapper
 {
 public:
-	static string DOTNET_MINIFY_JS(string content);
-	static string DOTNET_MINIFY_CSS(string content);
-	static string DOTNET_DOWNLOAD_STRING(string content);
+
+	//
+
+	static string DOTNET_MINIFY_JS(string& content);
+	static string DOTNET_MINIFY_CSS(string& content);
+
+	//
+
+	static void DOTNET_CONVERT_VIDEO_TO_WEBM(string& input_path, string& output_path, int& bitrate);
+
+	//
+
+	static string DOTNET_DOWNLOAD_STRING(string& content);
+
+	//
 };
