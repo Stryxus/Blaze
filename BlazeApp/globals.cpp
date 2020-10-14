9d0bf48b8138f52821a4870938f7885ea74d08c8
Connor Shearer <connor_shearer@outlook.com>
2020-10-14 03:47:53 +0100

    Fix dll load name mismatch

--- BlazeApp/globals.cpp
+++ BlazeApp/globals.cpp
@@ -8,6 +8,6 @@ string Globals::SPECIFIED_PROJECT_DIRECTORY_PATH = "";
 string Globals::SPECIFIED_PROJECT_DIRECTORY_PATH_WWWROOT = "";
 string Globals::SPECIFIED_PROJECT_DIRECTORY_SETTINGS_JSON_PATH = "";
 
-string Globals::LIB_NET_WRAPPER = "BlazeApp.NETWrapper.dll";
+string Globals::LIB_NET_WRAPPER = "NativeBridge.dll";
 string Globals::LIB_ZLIB = "zlibd.dll";
 string Globals::LIB_PNG = "libpng16d.dll";
