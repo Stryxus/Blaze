12b051341b20e14047f3d49eb8cfeb9c107f052a
Connor Shearer <connor_shearer@outlook.com>
2021-01-17 06:07:14 +0000

    Load libcurl dll

--- Blaze/blazeapp.cpp
+++ Blaze/blazeapp.cpp
@@ -20,7 +20,7 @@ int main(int argc, const char* argv[])
 		SetConsoleTitle(string_to_wstring_copy("Blaze - Working on: " + Globals::SPECIFIED_PROJECT_DIRECTORY_PATH).c_str());
 		Logger::log_info("Initializing...");
 		Logger::log_info("Loading Dependencies...");
-		load_libraries(vector<string> { Globals::LIB_NET_WRAPPER, Globals::LIB_ZLIB, Globals::LIB_PNG });
+		load_libraries(vector<string> { Globals::LIB_NET_WRAPPER, Globals::LIB_ZLIB, Globals::LIB_PNG, Globals::LIB_CURL });
 		Logger::log_info("Preparing data processors...");
 		if (!Settings::get_settings()) return -1;
 		Logger::set_log_color(Logger::COLOR::GREEN_FOREGROUND);
