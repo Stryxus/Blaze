de102110d698bdf6fc5297ac739ff8eb591d51a7
Connor Shearer <connor_shearer@outlook.com>
2021-01-17 05:38:00 +0000

    Create netutils

---
+++ Blaze/netutils.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "netutils.h"
+
+void download_file()
+{
+
+}
+
+void download_to_memory()
+{
+
+}
+
+//
+
+void get_headers()
+{
+
+}
---
+++ Blaze/netutils.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void download_file();
+void download_to_memory();
+
+//
+
+void get_headers();
