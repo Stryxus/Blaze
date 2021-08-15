843b838f2d4c185c2aaea8553595bc85ef2df24a
Connor Shearer <connor_shearer@outlook.com>
2021-08-15 13:55:05 +0100

    Fix header reference

--- Blaze/sassproc.hpp
+++ Blaze/sassproc.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
 void add_scss_for_minification(const char* from);
-void add_css_for_minification(const char* from);
+void add_scss_for_minification(const char* from);
 void minify_css(const char* to, const char* includePath, int precision);
