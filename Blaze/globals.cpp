7cfa3d5060a385d586390e31a0c96dccb9a72cc0
Connor Shearer <connor_shearer@outlook.com>
2021-08-15 14:10:40 +0100

    Fix Blaze.NET DLL name

--- Blaze/globals.cpp
+++ Blaze/globals.cpp
@@ -8,7 +8,7 @@ string Globals::SPECIFIED_PROJECT_DIRECTORY_PATH = "";
 string Globals::SPECIFIED_PROJECT_DIRECTORY_PATH_WWWROOT = "";
 string Globals::SPECIFIED_PROJECT_DIRECTORY_SETTINGS_JSON_PATH = "";
 
-string Globals::LIB_NET_WRAPPER = "Blaze.CLR.dll";
+string Globals::LIB_NET_WRAPPER = "Blaze.NET.dll";
 string Globals::LIB_ZLIB = "zlibd.dll";
 string Globals::LIB_PNG = "libpng16d.dll";
 string Globals::LIB_CURL = "libcurl-x64.dll";
